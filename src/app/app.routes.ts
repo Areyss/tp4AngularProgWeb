824db86d9faf6de6f8767a142cc9bd386d62866b
Areyss <arielryss@gmail.com>
2026-05-06 19:29:14 -0300

    feat: Se agrego el componente punto 2 con el array de productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 // Importamos tu componente del Punto 1
 import { Punto1Component } from './components/punto1/punto1';
+import { Punto2Component } from './components/punto2/punto2';
 
 export const routes: Routes = [
     // Cuando la URL sea 'punto1', mostrará tu slider
     { path: 'punto1', component: Punto1Component },
+    { path: 'punto2', component: Punto2Component },
     
     // Ruta por defecto (opcional): si la URL está vacía, va al punto 1
     { path: '', redirectTo: 'punto1', pathMatch: 'full' }
