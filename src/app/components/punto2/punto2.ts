e5fbcd3daf4512c50c146dea3650f7f696ee017c
Areyss <arielryss@gmail.com>
2026-05-09 16:44:43 -0300

    feat: Se agrego la ventana modal del carrito, junto con la funcionalidad de agregar y borrar productos

--- src/app/components/punto2/punto2.ts
+++ src/app/components/punto2/punto2.ts
@@ -1,20 +1,48 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Producto } from '../../models/producto';
 import { ProductoService } from '../../services/producto';
 
 
 @Component({
   selector: 'app-punto2',
-  imports: [CommonModule],
+  imports: [CommonModule, CurrencyPipe],
   templateUrl: './punto2.html',
   styleUrl: './punto2.css',
 })
 export class Punto2Component {
 
   productos: Array<Producto>;
+  carrito: Array<Producto> = [];
+  mostrarModal: boolean = false;
 
   constructor(private productoService: ProductoService) {
     this.productos = this.productoService.getProductos();
+  }
+
+  agregarAlCarrito(producto: Producto) {
+    const existe = this.carrito.find(p => p.nombre === producto.nombre);
+    if (!existe) {
+      this.carrito.push(producto);
+    }
+  }
+  estaEnCarrito(producto: Producto): boolean {
+    return this.carrito.includes(producto);
+  }
+  eliminarDelCarrito(producto: Producto) {
+    this.carrito = this.carrito.filter(p => p.nombre !== producto.nombre);
+  }
+
+  get total(): number {
+    // Calcula el total sumando los precios de los productos en el carrito
+    return this.carrito.reduce((total, producto) => total + producto.precio, 0);
+    //                          ^valor anterior    ^suma actual              ^valor inicial
+  }
+  abrirModal() {
+    this.mostrarModal = true;
+  }
+
+  cerrarModal() {
+    this.mostrarModal = false;
   }
 }
