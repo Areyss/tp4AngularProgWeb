d510876d03f9dadd09ae1cf86459716778800c9c
Areyss <arielryss@gmail.com>
2026-05-09 15:20:15 -0300

    feat: Se agrego el componente punto 2 con modelo y servicio de productos

--- src/app/components/punto2/punto2.ts
+++ src/app/components/punto2/punto2.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Producto } from '../../models/producto';
+import { ProductoService } from '../../services/producto';
 
-interface Producto {
-  nombre: string;
-  descripcion: string;
-  img: string;
-  precio: number;
-}
 
 @Component({
   selector: 'app-punto2',
@@ -16,12 +12,9 @@ interface Producto {
 })
 export class Punto2Component {
 
-  productos: Producto[] = [
-    { nombre: 'Notebook ASUS Zenbook S 16 16"', descripcion: 'AMD Ryzen AI 7 350 24GB DDR5 SSD 1TB Win11 3K OLED', img: 'assets/img/asus.jpg', precio: 2800250 },
-    { nombre: 'Monitor Gamer ASUS 22"', descripcion: 'FHD IPS 100Hz', img: 'assets/img/monitor.jpg', precio: 150750 },
-    { nombre: 'Teclado Mecanico ASUS ROG Strix M701', descripcion: 'RGB Wireless 2.4Ghz Bluetooth Switch NX Snow Black ESP', img: 'assets/img/teclado.jpg', precio: 294800 },
-    { nombre: 'Mouse SteelSeries Aerox 9', descripcion: 'Wireless 2.4Ghz Bluetooth Ultra LightWeight', img: 'assets/img/mouse.jpg', precio: 203550},
-    { nombre: 'Auriculares Corsair Virtuoso MAX', descripcion: 'Wireless 2.4Ghz Bluetooth Premium Dolby Atmos Black USB-C', img: 'assets/img/auriculares.jpg', precio: 676750 },
-    { nombre: 'Webcam Logitech Brio 4K UHD', descripcion: 'HDR 1080p/60FPS 720/90FPS USB-C', img: 'assets/img/webcam.jpg', precio: 252300},
-  ];
+  productos: Array<Producto>;
+
+  constructor(private productoService: ProductoService) {
+    this.productos = this.productoService.getProductos();
+  }
 }
